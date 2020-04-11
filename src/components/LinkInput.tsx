7e2ecb56567027904e3929ec28fad600dd752b54
Chris Finn <chris@finn.com>
2020-04-10 22:12:12 -0400

    Add Info button. Add version info.

--- src/components/LinkInput.tsx
+++ src/components/LinkInput.tsx
@@ -1,6 +1,16 @@
 import React from 'react';
-import { StyleSheet, View, Image, TextInput } from 'react-native';
+const { name: APP_NAME, version: APP_VERSION } = require('../../package.json');
+import {
+  StyleSheet,
+  View,
+  Image,
+  TextInput,
+  TouchableOpacity,
+  Alert,
+  Text,
+} from 'react-native';
 import normalize from '../utlities/responsive';
+import Icon from 'react-native-vector-icons/EvilIcons';
 
 type Props = {
   inputText: string;
@@ -9,23 +19,50 @@ type Props = {
 
 const defaultProps = Object.freeze({});
 
-export const LinkInput = ({ inputText, handleOnChangeText }: Props) => (
-  <View style={styles.container}>
-    <Image
-      style={styles.logo}
-      source={require('../../resources/logo_large.png')}
-    />
-    <View style={styles.inputContainer}>
-      <TextInput
-        style={styles.input}
-        placeholder="Paste link here to be Transposed"
-        placeholderTextColor="#808080"
-        onChangeText={text => handleOnChangeText(text)}
-        value={inputText}
+export const LinkInput = ({ inputText, handleOnChangeText }: Props) => {
+  const handleQuestionPress = () => {
+    Alert.alert(
+      'How To Use Tranpose',
+      `Transpose takes your music share link and creates a universal Transpose link for you to share. When someone clicks this Transpose link, it will open Transpose on their phone and display the share information and buttons to access the item in the available providers.
+
+Current supported providers:
+Spotify
+Apple Music
+        
+Current supported share items:
+Track
+Artist
+Album
+
+Tip: Instead of entering the share link here, you can save time by finding the Transpose app in the Share Sheet in your music provider and sharing from there.`,
+      [{ text: 'OK', onPress: () => {} }],
+    );
+  };
+
+  return (
+    <View style={styles.container}>
+      <Image
+        style={styles.logo}
+        source={require('../../resources/logo_large.png')}
       />
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.input}
+          placeholder="Paste link here to be Transposed"
+          placeholderTextColor="#808080"
+          onChangeText={text => handleOnChangeText(text)}
+          value={inputText}
+        />
+      </View>
+      <TouchableOpacity
+        style={styles.questionMark}
+        onPress={() => handleQuestionPress()}>
+        <Icon name="question" size={normalize(45)} color="#080808" />
+      </TouchableOpacity>
+      <Text style={styles.version}>{`v${APP_VERSION}`}</Text>
     </View>
-  </View>
-);
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -53,6 +90,15 @@ const styles = StyleSheet.create({
   },
   spacer: {
     flex: 1,
+  },
+  questionMark: {
+    position: 'absolute',
+    right: 0,
+    bottom: 20,
+  },
+  version: {
+    position: 'absolute',
+    bottom: 20,
   },
 });
 
